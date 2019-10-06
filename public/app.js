d2b8e3ae690c457b663424466ad5ee92443d9062
Garpken <gknafey@gmail.com>
2019-10-05 21:25:22 -0400

    Fixed the loading of the articles to load automatically without the reloading the page

--- public/app.js
+++ public/app.js
@@ -1,60 +1,68 @@
-$(document).ready(function() {
-    $.ajax({
-        method:"GET",
-        url: "/scrape"
-    }).then(function(data) {
-        console.log(data);
-        console.log("Scraping");
-    })
+$(document).ready(function () {
+  $.ajax({
+    method: "GET",
+    url: "/scrape"
+  }).then(function (data) {
+    console.log(data);
+    console.log("Scraping");
+
+    setTimeout(function(){getArticles();}, 2000);
+  })
+
 })
 
-$.getJSON("/articles", function(data) {
+
+function getArticles() {
+  
+
+  $.getJSON("/articles", function (data) {
     console.log(data);
+    $("#articles").empty();
     for (var i = 0; i < data.length; i++) {
       $("#articles").append("<p style='background-color:#D8E0BB; border-radius: 10px; padding: 10px;' data-id='" + data[i]._id + "'> <strong>" + data[i].title + "</strong><br />" + data[i].link + "<br>" + data[i].summary + "</p>");
     }
   });
-  
-  
-  $(document).on("click", "p", function() {
-    $("#notes").empty();
-    var thisId = $(this).attr("data-id");
-  
-    $.ajax({
-      method: "GET",
-      url: "/articles/" + thisId
-    })
-      .then(function(data) {
-        console.log(data);
-        $("#notes").append("<h2>" + data.title + "</h2>");
-        $("#notes").append("<input id='titleinput' name='title' >");
-        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
-        if (data.note) {
-          $("#titleinput").val(data.note.title);
-          $("#bodyinput").val(data.note.body);
-        }
-      });
-  });
-  
-  $(document).on("click", "#savenote", function() {
-    var thisId = $(this).attr("data-id");
-  
-    $.ajax({
-      method: "POST",
-      url: "/articles/" + thisId,
-      data: {
-        title: $("#titleinput").val(),
-        body: $("#bodyinput").val()
+}
+
+
+$(document).on("click", "p", function () {
+  $("#notes").empty();
+  var thisId = $(this).attr("data-id");
+
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+    .then(function (data) {
+      console.log(data);
+      $("#notes").append("<h2>" + data.title + "</h2>");
+      $("#notes").append("<input id='titleinput' name='title' >");
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      if (data.note) {
+        $("#titleinput").val(data.note.title);
+        $("#bodyinput").val(data.note.body);
       }
-    })
-      .then(function(data) {
-        console.log(data);
-        $("#notes").empty();
-      });
-  
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
-  });
-  
+    });
+});
+
+$(document).on("click", "#savenote", function () {
+  var thisId = $(this).attr("data-id");
+
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: $("#titleinput").val(),
+      body: $("#bodyinput").val()
+    }
+  })
+    .then(function (data) {
+      console.log(data);
+      $("#notes").empty();
+    });
+
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
--- server.js
+++ server.js
@@ -19,75 +19,76 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.connect(MONGODB_URI);
 
-app.get("/scrape", function(req, res) {
-    axios.get("https://www.thetimesinplainenglish.com").then(function(response) {
-        var $ = cheerio.load(response.data);
+app.get("/scrape", function (req, res) {
+  axios.get("https://www.thetimesinplainenglish.com").then(function (response) {
+    var $ = cheerio.load(response.data);
 
-        console.log(response.data);
-        $("div h2").each(function(i, element) {
+    console.log(response.data);
+    $("div h2").each(function (i, element) {
 
-            var result = {};
+      var result = {};
 
-            result.title = $(this)
-            .children("a")
-            .text();
-            result.link = $(this)
-            .children("a")
-            .attr("href");
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
 
-            result.summary = $(this)
-            .parent("div")
-            .children("div.entry")
-            .children("p")
-            .text();
+      result.summary = $(this)
+        .parent("div")
+        .children("div.entry")
+        .children("p")
+        .text();
 
 
-            db.Article.create(result)
-                .then(function(dbArticle) {
-                    console.log(dbArticle);
-                })
-                .catch(function(err) {
-                    console.log(err);
-                });
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          console.log(err);
         });
-        console.log("Scrape Complete");
     });
+    console.log("Scrape Complete");
+  });
+  res.redirect('/');
 });
 
-app.get("/articles", function(req, res) {
-    db.Article.find({})
+app.get("/articles", function (req, res) {
+  db.Article.find({})
     .then(results => res.json(results))
     .catch(err => console.log(err));
 })
 
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
 
-    var specific = req.params.id;
-  
-    db.Article.findOne({_id: specific}).populate("note").then(function(result) {
-      res.json(result);
-    })
-    .catch(function(err) {
+  var specific = req.params.id;
+
+  db.Article.findOne({ _id: specific }).populate("note").then(function (result) {
+    res.json(result);
+  })
+    .catch(function (err) {
       res.json(err);
     });
-  
-  });
-  
-  app.post("/articles/:id", function(req, res) {
-  
-    db.Note.create(req.body).then(function(dbNote) {
-      return db.Article.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, {new: true});
-    })
-    .then(function(dbArticle) {
+
+});
+
+app.post("/articles/:id", function (req, res) {
+
+  db.Note.create(req.body).then(function (dbNote) {
+    return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+  })
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     })
-  
-  });
-  
 
-  app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
-  });
+});
+
+
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
